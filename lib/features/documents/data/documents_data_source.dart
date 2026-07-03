0fd8d04a0821a7f303449d3f348127ee7cb11bd9
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-03 22:43:11 +0200

    feat: add documents data source contract

---
+++ lib/features/documents/data/documents_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:open_life_kit/features/documents/domain/important_document.dart';
+
+abstract class DocumentsDataSource {
+  Future<List<ImportantDocument>> loadDocuments();
+  Future<void> saveDocument(ImportantDocument document);
+}
