965585be0b45a001572b96225dfbb4df233c37bc
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-03 22:43:45 +0200

    feat: add reminders data source contract

---
+++ lib/features/reminders/data/reminders_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:open_life_kit/features/reminders/domain/local_reminder.dart';
+
+abstract class RemindersDataSource {
+  Future<List<LocalReminder>> loadReminders();
+  Future<void> saveReminder(LocalReminder reminder);
+}
