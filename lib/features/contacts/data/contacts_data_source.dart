fc7f4ece9694a0ffd0bc9419efae5273cefb703c
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-03 22:43:04 +0200

    feat: add contacts data source contract

---
+++ lib/features/contacts/data/contacts_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:open_life_kit/features/contacts/domain/important_contact.dart';
+
+abstract class ContactsDataSource {
+  Future<List<ImportantContact>> loadContacts();
+  Future<void> saveContact(ImportantContact contact);
+}
