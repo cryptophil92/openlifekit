07028408414f92ce7778800b290357012fe107b0
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 17:55:35 +0200

    feat: add contact remove contract

--- lib/features/contacts/data/contacts_data_source.dart
+++ lib/features/contacts/data/contacts_data_source.dart
@@ -3,4 +3,5 @@ import 'package:open_life_kit/features/contacts/domain/important_contact.dart';
 abstract class ContactsDataSource {
   Future<List<ImportantContact>> loadContacts();
   Future<void> saveContact(ImportantContact contact);
+  Future<void> removeContact(String id);
 }
