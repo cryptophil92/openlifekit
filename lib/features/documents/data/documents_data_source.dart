5aceecaec3ba7970fd3d8c33fbe4203630256daf
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 17:58:00 +0200

    feat: add docs forget contract

--- lib/features/documents/data/documents_data_source.dart
+++ lib/features/documents/data/documents_data_source.dart
@@ -3,5 +3,5 @@ import 'package:open_life_kit/features/documents/domain/important_document.dart'
 abstract class DocumentsDataSource {
   Future<List<ImportantDocument>> loadDocuments();
   Future<void> saveDocument(ImportantDocument document);
-  Future<void> removeDocument(String id);
+  Future<void> forgetDocument(String id);
 }
