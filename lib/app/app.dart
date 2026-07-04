b364eb3f387e9d785ab050d24e0156f6fb3114a5
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 02:19:02 +0200

    refactor: allow injecting router in app widget

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:open_life_kit/app/router.dart';
 import 'package:open_life_kit/app/theme.dart';
 import 'package:open_life_kit/core/constants/app_constants.dart';
@@ -8,7 +9,9 @@ import 'package:open_life_kit/features/settings/application/app_settings_provide
 import 'package:open_life_kit/features/settings/domain/app_theme_preference.dart';
 
 class OpenLifeKitApp extends ConsumerWidget {
-  const OpenLifeKitApp({super.key});
+  const OpenLifeKitApp({super.key, GoRouter? router}) : _router = router;
+
+  final GoRouter? _router;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -17,7 +20,7 @@ class OpenLifeKitApp extends ConsumerWidget {
     return MaterialApp.router(
       title: AppConstants.appName,
       debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
+      routerConfig: _router ?? appRouter,
       theme: buildLightTheme(),
       darkTheme: buildDarkTheme(),
       themeMode: themeModeFromPreference(settings.themePreference),
