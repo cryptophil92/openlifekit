9d3147063670e3a31f2fe2ed736381aa38bdd343
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 19:15:49 +0200

    feat: add reminders removal contract

--- lib/features/reminders/data/reminders_data_source.dart
+++ lib/features/reminders/data/reminders_data_source.dart
@@ -3,4 +3,5 @@ import 'package:open_life_kit/features/reminders/domain/local_reminder.dart';
 abstract class RemindersDataSource {
   Future<List<LocalReminder>> loadReminders();
   Future<void> saveReminder(LocalReminder reminder);
+  Future<void> removeReminder(String id);
 }
