8f3d44a0a576c1daed76d34025cfd8b605dfad58
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 17:58:50 +0200

    feat: add docs forget contract

--- lib/features/documents/data/memory_documents_data_source.dart
+++ lib/features/documents/data/memory_documents_data_source.dart
@@ -6,19 +6,24 @@ class MemoryDocumentsDataSource implements DocumentsDataSource {
     List<ImportantDocument> initialDocuments = const <ImportantDocument>[],
   ]) {
     for (final ImportantDocument document in initialDocuments) {
-      _documentsById[document.id] = document;
+      _items[document.id] = document;
     }
   }
 
-  final Map<String, ImportantDocument> _documentsById = <String, ImportantDocument>{};
+  final Map<String, ImportantDocument> _items = <String, ImportantDocument>{};
 
   @override
   Future<List<ImportantDocument>> loadDocuments() async {
-    return List<ImportantDocument>.unmodifiable(_documentsById.values);
+    return List<ImportantDocument>.unmodifiable(_items.values);
   }
 
   @override
   Future<void> saveDocument(ImportantDocument document) async {
-    _documentsById[document.id] = document;
+    _items[document.id] = document;
+  }
+
+  @override
+  Future<void> forgetDocument(String id) async {
+    _items.remove(id);
   }
 }
