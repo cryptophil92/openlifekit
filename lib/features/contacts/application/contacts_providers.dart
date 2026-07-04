c7e41c3ce74b1a2b13c6f67680282cef51af5553
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-04 16:27:43 +0200

    feat: wire contacts screen to data provider

--- lib/features/contacts/application/contacts_providers.dart
+++ lib/features/contacts/application/contacts_providers.dart
@@ -4,7 +4,16 @@ import 'package:open_life_kit/features/contacts/data/memory_contacts_data_source
 import 'package:open_life_kit/features/contacts/domain/important_contact.dart';
 
 final contactsDataSourceProvider = Provider<ContactsDataSource>(
-  (ref) => MemoryContactsDataSource(),
+  (ref) => MemoryContactsDataSource(
+    const <ImportantContact>[
+      ImportantContact(
+        id: 'contact-close',
+        category: ContactCategory.other,
+        displayName: 'Contact proche',
+        phone: 'Telephone a ajouter',
+      ),
+    ],
+  ),
 );
 
 final contactsProvider = FutureProvider<List<ImportantContact>>((ref) {
